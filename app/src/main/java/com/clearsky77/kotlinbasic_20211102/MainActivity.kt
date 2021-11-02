91b5939e70fc8efc5862da91ef463fb8ee37d261
clearsky77 <clearsky3264@gmail.com>
2021-11-02 11:48:41 +0900

    Toast 띄우기

--- app/src/main/java/com/clearsky77/kotlinbasic_20211102/MainActivity.kt
+++ app/src/main/java/com/clearsky77/kotlinbasic_20211102/MainActivity.kt
@@ -3,17 +3,26 @@ package com.clearsky77.kotlinbasic_20211102
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         btnLog.setOnClickListener {
-            Log.d("메인화면","로그찍기 버튼이 눌림")
-            Log.e("메인화면","에러")
-            Log.w("메인화면","워닝")
-            Log.wtf("메인화면","이 무슨 이런게!")
+            Log.d("메인화면", "로그찍기 버튼이 눌림")
+            Log.e("메인화면", "에러")
+            Log.w("메인화면", "워닝")
+            Log.wtf("메인화면", "이 무슨 이런게!")
+        }
+
+        btnToast.setOnClickListener {
+            Toast.makeText(this, "둥근 Toast 떴습니~다~♪", Toast.LENGTH_SHORT).show()
+        }
+        btnToast2.setOnClickListener {
+            Toast.makeText(this, "자리에서 일~어나서~♪", Toast.LENGTH_SHORT).show()
         }
     }
 
